c834104f8b769fd123398e80676a27c8f8713f58
HeatherKmtb <heather.kaymtb@gmail.com>
2021-01-08 16:32:06 +0000

    adjusting zonal_stats again

--- h_kay/zonal_stats.py
+++ h_kay/zonal_stats.py
@@ -12,10 +12,10 @@ from rasterstats import zonal_stats
 #gedi = '/Users/heatherkay/q_res/test/GEDI02_B_2019275183438_O04563_T02081_02_001_01_1.gpkg'
 #vector = geopandas.read_file(gedi, layer='BEAM0000')
 #raster = '/Users/heatherkay/q_res/layers/ESACCI-LC-L4-LCCS-Map-300m-P5Y-2010-v1.6.1.tif'
-stats = 'median'
-gedi = '/scratch/a.hek4/test/GEDI02_B_2019275183438_O04563_T02081_02_001_01_1.gpkg'
-vector = geopandas.read_file(gedi, layer='BEAM0000')
-raster = '/scratch/a.hek4/ESACCI-LC-L4-LCCS-Map-300m-P5Y-2010-v1.6.1.tif'
+#stats = 'median'
+#gedi = '/scratch/a.hek4/test/GEDI02_B_2019275183438_O04563_T02081_02_001_01_1.gpkg'
+#vector = geopandas.read_file(gedi, layer='BEAM0000')
+#raster = '/scratch/a.hek4/ESACCI-LC-L4-LCCS-Map-300m-P5Y-2010-v1.6.1.tif'
 
 # For calculating zonal statistics
 def get_zonal_stats(vector, raster, stats):
