917c26be4f14c62c4b1c2dc849c3b24b7c52bc15
HeatherKmtb <heather.kaymtb@gmail.com>
2021-01-20 15:46:34 +0000

    adjustment of join_shp_by_filename for processing

--- h_kay/join_shp_by_filename.py
+++ h_kay/join_shp_by_filename.py
@@ -10,7 +10,7 @@ import numpy as np
 import rsgislib.vectorutils
 import glob
 
-def join_shps(folderin, folderout):
+def join_shps_filename(folderin, folderout):
     """
     joins all data from shape files in list based on item in filename
     
@@ -21,3 +21,16 @@ def join_shps(folderin, folderout):
     for i in grid:
         fileList = glob.glob(folderin + 'gla14_file' + i + '_join*.shp')
         rsgislib.vectorutils.mergeShapefiles(fileList, folderout + 'gla14_grid_{}.shp'.format(i))
+
+def join_shps_grid(folderin, folderout):
+    """
+    joins all data from shape files in list based on item in filename
+    
+    
+    """
+    grid = np.arange(1, 22411, 1)
+    #grid = grid.astype(str)
+    for i in grid:
+        fileList = glob.glob(folderin + 'gla14_file*_join_eco_' + i + '.shp')
+        rsgislib.vectorutils.mergeShapefiles(fileList, folderout + 'gla14_grid_{}.shp'.format(i))
+   
