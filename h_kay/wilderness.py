1c161ff28520b7831e807a90b8f50468566436a4
HeatherKmtb <heather.kaymtb@gmail.com>
2021-02-02 14:23:04 +0000

    setting crs for split_files in wilderness.gla14_join

--- h_kay/wilderness.py
+++ h_kay/wilderness.py
@@ -74,6 +74,7 @@ def gla14_join(filein, folderout, folderin):
     for filename in split_files:
         basename = os.path.splitext(os.path.basename(filename))[0]
         base_gpd_df = gpd.read_file(filename)
+        base_gpd_df = base_gpd_df.set_geometry(col='geometry', crs="ESPG:4326")
         join_gpg_df = gpd.sjoin(base_gpd_df, join_gpg_df, how="inner", op="within")
         join_gpg_df.to_file(folderout, "{}_join.shp".format(basename))
         
