09ac6e5b55ac428a94b347603231571eeb2e40cf
HeatherKmtb <heather.kaymtb@gmail.com>
2021-02-03 15:13:08 +0000

    fixing wilderness.gla14_join

--- h_kay/wilderness.py
+++ h_kay/wilderness.py
@@ -70,11 +70,12 @@ def gla14_join(filein, folderout, folderin):
     split_files = glob.glob(folderin + '*.shp')
 
     join_gpg_df = gpd.read_file(filein)
+    join_gpg_df = join_gpg_df.set_crs(crs="EPSG:4326")
 
     for filename in split_files:
         basename = os.path.splitext(os.path.basename(filename))[0]
         base_gpd_df = gpd.read_file(filename)
-        base_gpd_df = base_gpd_df.set_geometry(col='geometry', crs="ESPG:4326")
+        base_gpd_df = base_gpd_df.set_geometry(col='geometry', crs="EPSG:4326")
         join_gpg_df = gpd.sjoin(base_gpd_df, join_gpg_df, how="inner", op="within")
         if join_gpg_df.empty:
             continue
