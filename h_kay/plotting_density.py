1b2a62a3831220cef43e0c77b4851cc0e3e7caa1
HeatherKmtb <heather.kaymtb@gmail.com>
2021-03-10 17:02:28 +0000

    cleaning final processing script in plotting_density

--- h_kay/plotting_density.py
+++ h_kay/plotting_density.py
@@ -246,4 +246,135 @@ def grid2(folderin, fileout, folderout, naming=4, eco_loc=2):
         plt.close     
         
 
-     
+      
+
+def grid_test(folderin, fileout, folderout, naming=4, eco_loc=2):
+    """
+    Function to compute q and provide results (csv) and figures (pdf)
+    
+    Parameters
+    ----------
+    
+    folderin: string
+            Filepath for folder with files ready for analysis
+                
+    naming: int
+          Section of filename to obtain ID (here grid number). Obtained
+          by splitting filename by '_' and indexing
+          Default = 3
+
+    eco_loc: int
+          Section of filename to obtain ecoregion (if applicable). 
+          Obtained as with naming
+          Default = 2
+          
+    fileout: string
+           Filepath for results file ending '.csv'
+           
+    folderout: string
+             Filepath for folder to save the figures            
+    """
+    df_id2 = pd.read_csv('./eco/final_ID.csv')
+    df_id = df_id2.astype({'ECO_ID': 'str'})
+    eco_ID = df_id.set_index('ECO_ID')['ECO_NAME'].to_dict()
+
+    #using 'file' to title plot  
+    fileList = glob.glob(folderin + '*.shp')
+
+    #create df for results
+    resultsa = pd.DataFrame(columns = ['eco', 'ID', 'qout', 'r_sq', 'deg_free', 'mse','r_sq_mean', 'adj_r2'])
+    #resultsb = pd.DataFrame(columns = ['eco', 'ID', 'qout', 'r_sq', 'deg_free', 'rmse'])
+
+    for file in fileList:
+        df = gpd.read_file(file)
+        if df.empty:
+            continue 
+        hd, tl = path.split(file)
+        shp_lyr_name = path.splitext(tl)[0]
+        name_comp = shp_lyr_name.split('_')
+        name = name_comp[naming] 
+        eco = name_comp[eco_loc]
+        eco_name = eco_ID[eco]
+        print(name)
+        print(eco)
+        #remove data with H_100 >= 0 prior to logging
+        test2 = df[df['i_h100']>=0] 
+        
+        #means x is just the h100 data - needs logging to normalise (not skewed) 
+        x = test2['i_h100']
+        
+        #create new column in df with log of H_100 
+        y = np.log(x)
+        test3 = test2.assign(log_i_h100 = y)
+        
+        if test3.empty:
+            continue
+
+        #get quantiles
+        a = np.quantile(test3['log_i_h100'],0.95)
+        #b = np.quantile(test2a['log_i_h100'],0.05)
+
+        #remove data outside of 5% quantiles
+        #test3 = test2a[test2a.log_i_h100 >b]
+        final = test3[test3.log_i_h100 <a]
+
+        if final.empty:
+            continue
+        del a, x, y, test2, test3
+
+        footprints = len(final['i_h100'])
+        
+        if footprints < 100:
+            continue
+            
+        #regression 
+        def f(x,q):
+            return 1- np.exp(-q * x)
+    
+        x = final['i_h100'].to_numpy()
+        y = final['i_cd'].to_numpy() 
+        x = np.append(x, [0])
+        y = np.append(y, [0])
+    
+        qout, qcov = curve_fit(f, x, y, 0.04)
+        qout = qout.round(decimals=4)
+        
+        y_predict = f(x, qout)
+            
+        from sklearn.metrics import mean_squared_error
+
+        mse = mean_squared_error(y, y_predict)
+
+        resultsa = resultsa.append({'eco': eco, 'ID': name, 'qout': qout, 
+                                    'deg_free': footprints, 
+                                    'mse': mse}, ignore_index=True)
+
+        resultsa.to_csv(fileout)
+
+        xy = np.vstack([x,y])
+        z = gaussian_kde(xy)(xy)
+
+        fig, ax = plt.subplots()
+        ax.scatter(x, y, c=z, s=10, edgecolor='')
+        plt.rcParams.update({'font.size':12}) 
+
+        ax.set_title('Grid square within' + eco_name)
+        ax.set_ylabel('Canopy Density')
+        ax.set_xlabel('Height - h100 (m)')
+        ax.set_xlim([0, 60])
+        ax.set_ylim([0,1])
+        #plotting regression
+        #putting x data in an order, cause that's what the code needs
+        xdata = np.linspace(0, 60)
+        #for each value of x calculating the corresponding y value
+        ycurve = [f(t, qout) for t in xdata]
+        #plotting the curve
+        ax.plot(xdata, ycurve, linestyle='-', color='red')
+        #adding qout, mse and deg_free to plot
+        #ax.annotate('adj_r2 = ' + str(adj_r2[0]), xy=(0.975,0.10), xycoords='axes fraction', fontsize=12, horizontalalignment='right', verticalalignment='bottom')
+        ax.annotate('q = ' + str(qout[0]), xy=(0.975,0.15), xycoords='axes fraction', fontsize=12, horizontalalignment='right', verticalalignment='bottom')
+        ax.annotate('MSE = ' + str(mse), xy=(0.975,0.10), xycoords='axes fraction', fontsize=12, horizontalalignment='right', verticalalignment='bottom')
+        ax.annotate('No of footprints = ' + str(footprints),xy=(0.975,0.05), xycoords='axes fraction', fontsize=12, horizontalalignment='right', verticalalignment='bottom')
+        plt.savefig(folderout + 'fig{}_{}.pdf'.format(eco, name))
+        plt.close     
+          
